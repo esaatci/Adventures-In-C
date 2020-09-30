954e2bf0775ef021da2d5e896ca90f4af180166f
esaatci <efesaatci2019@u.northwestern.edu>
2020-09-30 18:30:51 -0500

    finished sort program

--- c_book/chapter_5/function_pointers.c
+++ c_book/chapter_5/function_pointers.c
@@ -16,21 +16,42 @@ void my_qsort(void *lineptr[], int left, int right, int reverse,
 void swap(void *v[], int i, int j);
 int numcmp(char *s1, char *s2);
 int my_getline(char *buf, int len);
-
+int foldcomp(char *s1, char *s2);
 /* sort input lines */
+int nlines = 0;    /* number of input lines read */
+int numeric = 0;   /* 1 if numeric sort */
+int reverse = 0;   /* -1 if -r flag */
+int fold = 0;      /* 1 if fold cases together */
+int directory = 0; /* 1 if directory */
+
 int main(int argc, char *argv[]) {
-  int nlines;        /* number of input lines read */
-  int numeric = 0;   /* 1 if numeric sort */
-  int reverse = 1;   /* -1 if -r flag */
-  int fold = 0;      /* 1 if fold cases together */
-  int directory = 0; /* 1 if directory */
+  int i;
+  void *fun;
 
   if (argc > 1 && strcmp(argv[1], "-n") == 0)
     numeric = 1;
 
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-n") == 0) {
+      numeric = 1;
+    } else if (strcmp(argv[i], "-r") == 0) {
+      reverse = -1;
+    } else if (strcmp(argv[i], "-f")) {
+      fold = 1;
+    } else if (strcmp(argv[i], "-d")) {
+      directory = 1;
+    }
+  }
+
+  if (numeric) {
+    fun = numcmp;
+  } else if (fold || directory) {
+    fun = foldcomp;
+  } else {
+    fun = strcmp;
+  }
   if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-    my_qsort((void **)lineptr, 0, nlines - 1, reverse,
-             (int (*)(void *, void *))(numeric ? numcmp : strcmp));
+    my_qsort((void **)lineptr, 0, nlines - 1, reverse, (int (*)(void *, void *))fun);
     writelines(lineptr, nlines);
     return 0;
   } else {
@@ -47,15 +68,14 @@ void my_qsort(void *v[], int left, int right, int reverse,
     return;
   }
   swap(v, left, (left + right) / 2);
-
   last = left;
   for (i = left + 1; i <= right; i++) {
     if ((((*comp)(v[i], v[left])) * reverse) < 0) {
       swap(v, ++last, i);
     }
     swap(v, left, last);
-    qsort(v, left, last - 1, comp);
-    qsort(v, left + 1, right, comp);
+    my_qsort(v, left, last - 1, reverse, comp);
+    my_qsort(v, left + 1, right, reverse, comp);
   }
 }
 
@@ -73,10 +93,37 @@ int numcmp(char *s1, char *s2) {
     return 0;
 }
 
-/* No error checking */
+/*
+  No error checking
+  works with both fold and comp
+*/
 int foldcomp(char *s1, char *s2) {
   /* write recursive function */
-  return 0;
+  char temp1, temp2;
+  if (*s1 == '\0' && *s2 == '\0') {
+    return 0;
+  }
+  if (*s1 == '\0' && *s2 != '\0') {
+    return -1;
+  }
+  if (*s1 != '\0' && *s2 == '\0') {
+    return 1;
+  }
+  if (directory) {
+    temp1 = (isalnum(*s1) || isblank(*s1)) ? *s1 : 0;
+    temp2 = (isalnum(*s2) || isblank(*s2)) ? *s2 : 0;
+  }
+  if (fold) {
+    temp1 = isalpha(*s1) ? tolower(*s1) : *s1;
+    temp2 = isalpha(*s2) ? tolower(*s2) : *s2;
+  }
+  if (temp1 > temp2) {
+    return 1;
+  } else if (temp1 < temp2) {
+    return -1;
+  } else {
+    return foldcomp(s1 + 1, s2 + 1);
+  }
 }
 
 void swap(void *v[], int i, int j) {
