bc1f4b577fedc445b100fc89ece0b0975600ba2a
esaatci <efesaatci2019@u.northwestern.edu>
2020-11-02 09:19:59 -0600

    implement file comparator

--- c_book/chapter_7/file_comp/main.c
+++ c_book/chapter_7/file_comp/main.c
@@ -2,16 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* Exercise 7-6. Write a program to compare two files, printing the first line where they differ. */
+
+#define LINE 256
+/*
+Exercise 7-6. Write a program to compare
+two files, printing the first line where they differ.
+*/
 
 int main(int argc, char **argv) {
-  char *prog = *argv;
+  char *file1, *file2, *prog;
+  char *c1, *c2;
+  char l1[LINE], l2[LINE];
+  int cmp_res;
 
+  prog = *argv;
   if (argc < 3) {
-    fprintf(stderr, "%s not enough args\n" ,prog);
+    fprintf(stderr, "%s not enough args\n", prog);
     exit(1);
   } else if (argc > 3) {
     fprintf(stderr, "%s alot of args\n", prog);
+    exit(1);
   }
+
+  file1 = argv[1];
+  file2 = argv[2];
+
+  /* open the files */
+  FILE *f1 = fopen(file1, "r");
+  FILE *f2 = fopen(file2, "r");
+  if (!f1 || !f2) {
+    fprintf(stderr, "can't open files\n");
+    exit(1);
+  }
+  /* start comparing them */
+  while ((c1 = fgets(l1, LINE, f1)) != NULL &&
+          (c2 = fgets(l2, LINE, f2)) != NULL) {
+    cmp_res = strcmp(l1, l2);
+    if (cmp_res < 0) {
+      printf("%s", l2);
+      exit(0);
+    } else if (cmp_res > 0) {
+      printf("%s", l1);
+      exit(0);
+    }
+  }
+  /* if file 2 is shorter print the last line in file 1 */
+  if ((c1 = fgets(l1, LINE, f1)) != NULL) {
+      printf("%s", l1);
+      exit(0);
+  }
+  /* if file 1 is shorter print the last line in file 2 */
+  if ((c2 = fgets(l2, LINE, f2)) != NULL) {
+      printf("%s", l2);
+      exit(0);
+  }
+  /* close the files */
+  fclose(f1);
+  fclose(f2);
+
+  printf("the files are identical\n");
   exit(0);
 }
---
+++ c_book/chapter_7/file_comp/test_files/disjunctive_normal_form.py
@@ -0,0 +1,87 @@
+import copy
+
+# for given n variables print their disjunctive normal form
+# input n number 
+# output string 
+
+"""
+    Example
+    n == 2
+    (a0 & a1) | (a0 & ~a1) | (~a0 & a1) | (~a0 & ~a1)
+
+
+"""
+
+variable = "{}a{}"
+statement = "({} & {})"
+
+p = variable.format("",0)
+
+# q = variable.format("", 1)
+# s = statement.format(p,q)
+
+# print(s)
+
+def print_disjunctive_form(n):
+
+    var_list = []
+    statements = []
+    for i in range(n):
+        var_list.append(["a{}".format(i), "~a{}".format(i)])
+    
+    helper(var_list, 0, statements, [])
+    
+    fun = lambda x: " & ".join(x)
+    fun2 = lambda x: "({})".format(x)
+    statements = map(fun, statements)
+    statements = map(fun2, statements)
+    return " | ".join(statements)
+
+
+
+
+    
+
+# Do we want 0 or 1
+
+def helper(v_list, i, statements, statement):
+
+    if i == len(v_list):
+        t = copy.copy(statement)
+        statements.append(t)
+        return
+    
+    statement.append(v_list[i][0])
+    helper(v_list, i+1, statements, statement)
+    statement.pop()
+    statement.append(v_list[i][1])
+    helper(v_list, i+1, statements, statement)
+    statement.pop()
+
+
+print(print_disjunctive_form(5))
+
+
+def print_truth_table(n):
+    values = []
+    helper_2(n, 0, values, [])
+    return values
+
+
+def helper_2(limit, i, statements, statement):
+
+    if i == limit:
+        t = copy.copy(statement)
+        t.append("\n")
+        statements.append(t)
+        return
+
+    statement.append("0")
+    helper_2(limit, i+1, statements, statement)
+    statement.pop()
+    statement.append("1")
+    helper_2(limit, i+1, statements, statement)
+    statement.pop()
+
+
+
